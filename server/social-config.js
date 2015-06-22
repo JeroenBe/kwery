209535b02783ae8a3b94ce9e197ca7188d74247e
Jeroen <jeroen.berrevoets@gmail.com>
2015-06-22 16:45:54 +0200

    added service

---
+++ server/social-config.js
@@ -0,0 +1,8 @@
+ServiceConfiguration.configurations.remove({
+  service: "facebook"
+});
+ServiceConfiguration.configurations.insert({
+  service: "facebook",
+  appId: "919078874816958",
+  secret: "fbcacec943a6c805c8d432c1b60dbe7d"
+});
