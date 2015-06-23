6a409e594c268ee7f408e333f3d5aaf0d691dc75
Jeroen <jeroen.berrevoets@gmail.com>
2015-06-23 14:01:22 +0200

    small performance improvement (for future API calls)

--- server/FBGraphRequests.js
+++ server/FBGraphRequests.js
@@ -1,9 +1,9 @@
+var sync = Meteor.wrapAsync(FBGraph.get)
+
 Meteor.methods({
 	'getEvents': function(){
 		var facebook = Meteor.users.findOne(this.userId).services.facebook
 		var params = {access_token: facebook.accessToken}
-
-		var sync = Meteor.wrapAsync(FBGraph.get)
 
 		var result = sync((facebook.id + '/events'), params)
 		console.log(result)
