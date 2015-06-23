a2c266226a6d744797ef9469c37783ab87c8818d
Jeroen <jeroen.berrevoets@gmail.com>
2015-06-23 14:50:27 +0200

    fixed paging in fb API requests; solves issue #1

--- client/view/home/home.js
+++ client/view/home/home.js
@@ -1,17 +1,18 @@
 Template.home.onCreated(function (){
 	this.fbevents = new ReactiveVar()
 	var self = this
-
+	var nextUrl
 	if(Meteor.userId()){
 		Meteor.call('getEvents', function(e, r){
 			if(e){
 				console.log(e)
 			}else{
-				self.fbevents.set(r.data)
+				self.fbevents.set(r)
 			}
 		})
 	}
 })
+
 
 
 Template.home.helpers({
--- server/FBGraphRequests.js
+++ server/FBGraphRequests.js
@@ -5,8 +5,19 @@ Meteor.methods({
 		var facebook = Meteor.users.findOne(this.userId).services.facebook
 		var params = {access_token: facebook.accessToken}
 
+		//First page
 		var result = sync((facebook.id + '/events'), params)
-		console.log(result)
-		return result
+
+		var data = result.data
+		var paging = result.paging
+
+		//Following pages
+		while(paging){
+			var tempRes = HTTP.get(paging.next)
+			data  = data.concat(tempRes.data.data)
+			paging = tempRes.paging
+		}
+
+		return data
 	}
 })
